36e03e5418c53d467a3ce0332004bafe92cd2dc5
Samantha Montgomery <montgomerysamantha5@gmail.com>
2018-11-30 14:06:32 -0600

    Added the uiOpenFileDialog and can now load in files

--- SVTTool/View.cs
+++ SVTTool/View.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SVTTool;
 
 namespace SVTTool
 {
@@ -20,6 +21,27 @@ namespace SVTTool
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void OpenFileClick(object sender, EventArgs e)
+        {
+            uiOpenFileDialog.Filter = "CSV files | *.csv";
+            if (uiOpenFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new
+                   System.IO.StreamReader(uiOpenFileDialog.FileName);
+                //read in the file line by line
+                StringBuilder sb = new StringBuilder();
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    sb.Append(line);
+                }
+                uiDisplayTextBox.Text = null;
+                uiDisplayTextBox.Text = sb.ToString();
+                sr.Close();
+                MessageBox.Show("Read in file");
+            }
         }
     }
 }
